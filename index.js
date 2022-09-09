db8ab04849bf4740021cb1734bdf7803d1f107e8
Peter <p33tcz@gmail.com>
2022-09-10 01:24:16 +0200

    🪐 INIT

---
+++ index.js
@@ -0,0 +1,136 @@
+#!/usr/bin/env node
+process.removeAllListeners('ExperimentalWarning')
+
+
+// Property of itsP33t on github. This script is a fork of EasyFivem by itsP33t
+
+// imports
+import path from 'path'
+import fs from 'fs-extra'
+import chalk from 'chalk';
+import cliSelect from 'cli-select'
+import inquirer from 'inquirer'
+import boxen from 'boxen';
+
+
+// variables
+import packageJson from './package.json' assert {type: 'json'}
+const ver = packageJson.version
+let qnt
+const ScriptLocation = path.dirname(process.argv[1]);
+let addonLoc = 'NOT SET'  
+
+
+function GetOS() {
+var osValue = process.platform;
+if (osValue == 'darwin') {
+    addonLoc = `${ScriptLocation}/addon`
+    return
+}else if(osValue == 'win32'){
+  addonLoc = `${ScriptLocation}/addon`
+  return
+}else if(osValue== 'android') {
+  addonLoc = `${ScriptLocation}/addon`
+    return
+}else if(osValue== 'linux') {
+  UsingLinux()
+  return
+}
+else{
+    console.log("OS not detected, you may have problems")
+    addonLoc = `${ScriptLocation}/addon`
+    return
+}
+}
+
+// generate random number
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function UsingLinux() {
+if (fs.existsSync(path.join(ScriptLocation, 'addon'))) {
+  addonLoc = `${ScriptLocation}/addon`
+    return
+}
+if (!fs.existsSync(path.join(ScriptLocation, 'addons'))) {
+console.log(chalk.red("Linux detected, but addons folder not found"))
+console.log(chalk.red.bold(`
+There's no auto fix for it right now.
+Follow this link to know how to fix it: https://github.com/itsP33t/EasyFivem/wiki/Fix-for-linux
+Everything except the name should be the same
+--------------------------------------------
+npm location: ${ScriptLocation}
+--------------------------------------------
+`))
+process.exit()
+}
+}
+
+
+GetOS() //.then(() => {
+console.log(`
+███████╗███████╗███╗   ██╗███████╗████████╗
+╚══███╔╝██╔════╝████╗  ██║██╔════╝╚══██╔══╝
+  ███╔╝ █████╗  ██╔██╗ ██║█████╗     ██║   
+ ███╔╝  ██╔══╝  ██║╚██╗██║██╔══╝     ██║   
+███████╗███████╗██║ ╚████║███████╗   ██║   
+╚══════╝╚══════╝╚═╝  ╚═══╝╚══════╝   ╚═╝   
+                 ✨CLI✨
+`)
+
+// Start
+console.log(boxen(`Current version: ${chalk.yellow(ver)}
+${chalk.green('You can import your own projects, just import the folder to')} ${chalk.gray(`${addonLoc}`)}`, {title: `Zenet official CLI`, titleAlignment: 'center', borderColor: 'white', borderStyle: 'round'}))
+console.log(`${chalk.blue('Select the template you want to use. Use arrows to select and enter to confirm',)}`)
+
+
+// just a form that will get a project name
+async function GetName() {
+  const answers = await inquirer.prompt({
+    name: 'name',
+    type: 'string',
+    message: 'What should be the name of the project?',
+    default() {
+      return randomNumber(1111, 9999)+'_Project';
+    },
+  });
+  qnt = answers.name
+}
+
+
+if (!fs.existsSync(path.join(ScriptLocation, '/addon'))) {
+  console.log(chalk.red("addon folder not found, try reinstalling the Zenet CLI, if it still doesn't work, create an issue on github"))
+  process.exit()
+}
+
+// this will fetch all the templates
+let folders = fs.readdirSync(`${addonLoc}`).filter(file => fs.statSync(path.join(`${addonLoc}`, file)).isDirectory());
+let foldersObject = {};
+folders.forEach(folder => {
+    foldersObject[folder] = folder;
+}
+);
+
+cliSelect({
+    values: foldersObject,
+    valueRenderer: (value, selected) => {
+        if (selected) {
+            return chalk.underline(value);
+        }
+        return value;
+    },
+}).then(value => {
+GetName().then(() => {
+let src = `${addonLoc}/${value.value}`;
+let dest = `./${qnt}`
+fs.copy(src, dest, function (err) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`${chalk.green('✅ Success! ')} ${chalk.yellow('Your project has been created!')}`);
+  }
+
+});
+
+})})
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Zenet CLI
+## What is it used for?
+### Zenet CLI is used to scaffold nodejs projects. You can import your own templates into the **/addon** folder
+
+## **This CLI is in early alpha, use at your own risk**
+
+Using:
+```
+npm i zenet -g
+```
+after installing, just type zenet into your cmd and our CLI should show up
